352181db899dff6f34ee727e7d87a3dd0616d7ef
Bladeo8x <gerardo50_@hotmail.com>
2022-10-28 15:25:15 -0500

    adding Menu file

--- src/models/Menu.js
+++ src/models/Menu.js
@@ -1,6 +1,5 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class menu extends Model {}
 
@@ -12,37 +11,34 @@ menu.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    dishName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    dishDesc: {
+    dish_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    dishPrice: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    dishType: {
-        type: DataTypes.INTEGER,
-        allowNull: false,    
-    },
-    dishImg: {
+    dish_desc: {
       type: DataTypes.STRING,
       allowNull: false,
-    }
+    },
+    dish_price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    dish_type: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    dish_img: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
-  
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'menu',
+    modelName: "menu",
   }
 );
 
 module.exports = menu;
-
-
